5ec666abe050df7aa6b51a603d0c93f271f2f93d
smmuja <smmuja27@gmail.com>
2024-09-30 17:24:05 +0700

    model: adjust user detail type and schema

--- src/models/UserAccount.js
+++ src/models/UserAccount.js
@@ -10,17 +10,28 @@ const UserAccountSchema = new mongoose.Schema(
     },
     first_name: {
       type: String,
+      default: null,
       trim: true,
     },
     last_name: {
       type: String,
+      default: null,
       trim: true,
     },
     bio: {
       type: String,
+      default: null,
     },
     phone_number: {
-      type: Number,
+      type: String,
+      match: [/^[0-9]+$/, "Phone number should only contain digits"],
+      unique: [true, "phone number is already in use"],
+      default: null,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "rather not say"],
+      default: null,
     },
   },
   { timestamps: true }
